9b2f7445259d26a1d536579d2db43cb7cd1f8b69
Kamil Moskała <moskalakamil07@gmail.com>
2024-12-04 09:42:22 +0000

    first commit

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,4 +1,27 @@
+use std::cell::RefCell;
+
+thread_local! {
+    static MSG: RefCell<String> = RefCell::new(String::new());
+}
+
+#[ic_cdk::update]
+fn set_msg(name: String) -> String {
+    //  COUNTER.with(|count| *count.borrow_mut() = n);
+    MSG.with(|msg| {
+        *msg.borrow_mut() = new_msg;
+    })
+}
+
+#[ic_cdk::query]
+fn get_msg() -> String {
+    MSG.with(|msg| msg.borrow().clone());
+}
+
 #[ic_cdk::query]
 fn greet(name: String) -> String {
+    let a = String::from("Siema");
+    let b = &a;
+    let c = a;
+
     format!("Hello, {}!", name)
 }
